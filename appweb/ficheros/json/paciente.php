3ebc75c0a73fc3a89feb1115c0cbf7a5f9496fce
luismrtic <luismr.tic@gmail.com>
2021-01-29 12:54:01 +0100

    objeto paciente getters y setters

--- appweb/ficheros/json/paciente.php
+++ appweb/ficheros/json/paciente.php
@@ -14,7 +14,43 @@ class Paciente{
         $this->resultados = $resultados;
     }
 
-    
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    public function getDni()
+    {
+        return $this->dni;
+    }
+
+    public function getResultados()
+    {
+        return $this->resultados;
+    }
+
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+    }
+
+    public function setDni($dni)
+    {
+        $this->nombre = $dni;
+    }
+
+    public function setResultados($resultados)
+    {
+        $this->nombre = $resultados;
+    }
+
+
+
+
+
+
+
+
 }
 
 
